ee08fb9a43724c416e8e9b0329423cf8d38c3463
EulerRochaDev <eulerrocha69gmail.com>
2025-11-04 18:03:39 -0300

    Corrigido com Boas Praticas

--- DESAFIO DE PROJETO/index.js
+++ DESAFIO DE PROJETO/index.js
@@ -1,20 +1,20 @@
 let Elo = "";
 let nome = "Volsung";
-let Xp = 2000;
+let Xp = 10001;
 
 if (Xp <= 1000) {
     Elo = "Ferro";
-} else if (Xp > 1001 && Xp <= 2000) {
+} else if (Xp >= 1001 && Xp <= 2000) {
     Elo = "Bronze";
-} else if (Xp > 2001 && Xp <= 5000) {
+} else if (Xp >= 2001 && Xp <= 5000) {
     Elo = "Prata";
-} else if (Xp > 5001 && Xp <= 7000) {
+} else if (Xp >= 5001 && Xp <= 7000) {
     Elo = "Ouro";
-} else if (Xp > 7001 && Xp <= 8000) {
+} else if (Xp >= 7001 && Xp <= 8000) {
     Elo = "Platina";
-} else if (Xp > 8001 && Xp <= 9000) {
+} else if (Xp >= 8001 && Xp <= 9000) {
     Elo = "Ascendente";
-} else if (Xp > 9001 && Xp <= 10000) {
+} else if (Xp >= 9001 && Xp <= 10000) {
     Elo = "Imortal";
 } else if (Xp >= 10001) {
     Elo = "Radiante";
