1af0f1c54d3db8b7664e9e3b119bb147264c4c55
EulerRochaDev <eulerrocha69gmail.com>
2025-11-04 17:57:57 -0300

    Desafio concluido

--- DESAFIO DE PROJETO/index.js
+++ DESAFIO DE PROJETO/index.js
@@ -1,1 +1,24 @@
-console.log("Hello, world!");
+let Elo = "";
+let nome = "Volsung";
+let Xp = 2000;
+
+if (Xp <= 1000) {
+    Elo = "Ferro";
+} else if (Xp > 1001 && Xp <= 2000) {
+    Elo = "Bronze";
+} else if (Xp > 2001 && Xp <= 5000) {
+    Elo = "Prata";
+} else if (Xp > 5001 && Xp <= 7000) {
+    Elo = "Ouro";
+} else if (Xp > 7001 && Xp <= 8000) {
+    Elo = "Platina";
+} else if (Xp > 8001 && Xp <= 9000) {
+    Elo = "Ascendente";
+} else if (Xp > 9001 && Xp <= 10000) {
+    Elo = "Imortal";
+} else if (Xp >= 10001) {
+    Elo = "Radiante";
+}
+
+console.log(`O jogador ${nome} possui ${Xp} pontos de experiência e seu elo é ${Elo}.`);
+
